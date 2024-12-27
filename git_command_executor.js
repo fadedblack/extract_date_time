8bb84cbe544e97fb3d6f58f28cb43d6cbdc09f33
Inkeet C <inkeetcha234@gmail.com>
2024-12-27 22:56:24 +0530

    refactor commandsToExecute to take command and execute

--- git_command_executor.js
+++ git_command_executor.js
@@ -1,44 +1,51 @@
 const gitLocations = [
-  "/Users/inkeetc/Dev/javascript//assignments/weekend_stuffs/rover_assignment/",
-  "/Users/inkeetc/Dev/javascript//assignments/weekend_stuffs/zebra_assignment/",
-  "/Users/inkeetc/Dev/javascript//assignments/weekend_stuffs/function_stuffs/",
-  "/Users/inkeetc/Dev/javascript//assignments/power_set/",
-  "/Users/inkeetc/Dev/javascript//assignments/number_to_words/",
-  "/Users/inkeetc/Dev/javascript//assignments/organizing_data/",
-  "/Users/inkeetc/Dev/javascript//assignments/patterns/",
-  "/Users/inkeetc/Dev/javascript//assignments/guess_the_number/",
-  "/Users/inkeetc/Dev/javascript//assignments/arrays/array_assignments1/",
-  "/Users/inkeetc/Dev/javascript//assignments/arrays/array_assignments2/",
-  "/Users/inkeetc/Dev/javascript//assignments/matrix_multiplication/",
-  "/Users/inkeetc/Dev/javascript//test/functions/",
-  "/Users/inkeetc/Dev/javascript//projects/snakeAndLadder/",
-  "/Users/inkeetc/Dev/javascript//projects/loki/",
-  "/Users/inkeetc/Dev/javascript//projects/calculator/",
-  "/Users/inkeetc/Dev/javascript//projects/Enigma/",
-  "/Users/inkeetc/Dev/javascript//projects/ticktactoe/",
-  "/Users/inkeetc/Dev/javascript//projects/labyrinth/",
-  "/Users/inkeetc/Dev/javascript//scripts/closure/",
-  "/Users/inkeetc/Dev/javascript//scripts/shuffle/",
-  "/Users/inkeetc/Dev/javascript//scripts/testing_framework/",
-  "/Users/inkeetc/Dev/javascript//scripts/multiplication_table/",
-  "/Users/inkeetc/Dev/javascript//scripts/animation/",
-  "/Users/inkeetc/Dev/javascript//scripts/objects/",
-  "/Users/inkeetc/Dev/javascript//scripts/is_number_power_of_2/",
-  "/Users/inkeetc/Dev/javascript//scripts/scripts/",
-  "/Users/inkeetc/Dev/javascript//scripts/sorting/",
-  "/Users/inkeetc/Dev/javascript//scripts/map_filter_reduce/",
-  "/Users/inkeetc/Dev/javascript//scripts/pattern/",
-  "/Users/inkeetc/Dev/javascript//scripts/map_filter_reduce_practice/",
+  "/Users/inkeetc/workspace/javascript//assignments/weekend_stuffs/rover_assignment/",
+  "/Users/inkeetc/workspace/javascript//assignments/weekend_stuffs/zebra_assignment/",
+  "/Users/inkeetc/workspace/javascript//assignments/weekend_stuffs/function_stuffs/",
+  "/Users/inkeetc/workspace/javascript//assignments/power_set/",
+  "/Users/inkeetc/workspace/javascript//assignments/number_to_words/",
+  "/Users/inkeetc/workspace/javascript//assignments/organizing_data/",
+  "/Users/inkeetc/workspace/javascript//assignments/patterns/",
+  "/Users/inkeetc/workspace/javascript//assignments/guess_the_number/",
+  "/Users/inkeetc/workspace/javascript//assignments/arrays/array_assignments1/",
+  "/Users/inkeetc/workspace/javascript//assignments/arrays/array_assignments2/",
+  "/Users/inkeetc/workspace/javascript//assignments/matrix_multiplication/",
+  "/Users/inkeetc/workspace/javascript//test/functions/",
+  "/Users/inkeetc/workspace/javascript//projects/snakeAndLadder/",
+  "/Users/inkeetc/workspace/javascript//projects/loki/",
+  "/Users/inkeetc/workspace/javascript//projects/calculator/",
+  "/Users/inkeetc/workspace/javascript//projects/Enigma/",
+  "/Users/inkeetc/workspace/javascript//projects/ticktactoe/",
+  "/Users/inkeetc/workspace/javascript//projects/labyrinth/",
+  "/Users/inkeetc/workspace/javascript//scripts/closure/",
+  "/Users/inkeetc/workspace/javascript//scripts/shuffle/",
+  "/Users/inkeetc/workspace/javascript//scripts/testing_framework/",
+  "/Users/inkeetc/workspace/javascript//scripts/multiplication_table/",
+  "/Users/inkeetc/workspace/javascript//scripts/animation/",
+  "/Users/inkeetc/workspace/javascript//scripts/objects/",
+  "/Users/inkeetc/workspace/javascript//scripts/is_number_power_of_2/",
+  "/Users/inkeetc/workspace/javascript//scripts/scripts/",
+  "/Users/inkeetc/workspace/javascript//scripts/sorting/",
+  "/Users/inkeetc/workspace/javascript//scripts/map_filter_reduce/",
+  "/Users/inkeetc/workspace/javascript//scripts/pattern/",
+  "/Users/inkeetc/workspace/javascript//scripts/map_filter_reduce_practice/",
+  "/Users/inkeetc/workspace/javascript//scripts/all_commit_logs/",
 ];
 
-const commandToExecute = gitLocations.map(function (location) {
-  const timeStamps = "cut -d' ' -f4-8";
-  const stdOut = "all_commit_timestamps.txt";
-  const filterByDate = " grep 'Date:'";
+const commandsToExecute = function (filter, fields, stdOut) {
+  return gitLocations.map(function (location) {
+    return `git -C ${location} log | ${filter} | ${fields} >> ${stdOut}`;
+  });
+};
 
-  return `git -C ${location} log | ${filterByDate} | ${timeStamps} >> ${stdOut}`;
-});
+const displayCommands = function (commandsList) {
+  commandsList.forEach((command) => console.log(command));
+};
 
-for (const command of commandToExecute) {
-  console.log(command);
-}
+const timeStampCommands = commandsToExecute(
+  "grep 'Date:'",
+  "cut -d' ' -f4-8",
+  "timestamps.txt"
+);
+
+displayCommands(timeStampCommands);
